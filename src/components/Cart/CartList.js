bd3047b46e0fc4edea1628b158f754866e1dafdd
Marcos Costa <50223741+marcoscosta90@users.noreply.github.com>
2020-12-01 15:50:56 -0300

    Ecommerce Project

---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+export default function CartList({value}) {
+    const {cart} = value;
+    console.log(value, cart);
+    
+    return (
+        <div>
+            Hello from cart list
+            <CartItem/>
+        </div>
+    );
+}
